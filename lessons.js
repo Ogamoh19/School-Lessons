4cb3c5f0c82f4978737f6757f0b469c9b58dea13
Ogamoh <mohdnagona@gmail.com>
2022-03-14 14:01:00 +0100

    Lessons displayed using array of JSON objects

---
+++ lessons.js
@@ -0,0 +1,12 @@
+let subjects = [
+    {"id": 1, "image": "img/Math.svg", "name": "Mathematics", "location": "London", "price": {"GBP": 150}, "spaces": 5},
+    {"id": 2, "image": "./img/English.svg", "name": "English", "location": "Manchester", "price": {"GBP": 170}, "spaces": 5},
+    {"id": 3, "image": "./img/Biology.svg", "name": "Biology", "location": "Northinham", "price": {"GBP": 200}, "spaces": 5},
+    {"id": 4, "image": "./img/Economics.svg", "name": "Economics", "location": "Birmingham", "price": {"GBP": 140}, "spaces": 5},
+    {"id": 5, "image": "./img/Commerce.svg", "name": "Commerce", "location": "Leicester", "price": {"GBP": 100}, "spaces": 5},
+    {"id": 6, "image": "./img/Literature.svg", "name": "Literature", "location": "Liverpool", "price": {"GBP": 90}, "spaces": 5},
+    {"id": 7, "image": "./img/Chemistry.svg", "name": "Chemistry", "location": "Bristol", "price": {"GBP": 110}, "spaces": 5},
+    {"id": 8, "image": "./img/Marketing.svg", "name": "Marketing", "location": "Cambridge", "price": {"GBP": 115}, "spaces": 5},
+    {"id": 9, "image": "./img/ICT.svg", "name": "ICT", "location": "Leeds", "price": {"GBP": 250}, "spaces": 5},
+    {"id": 10, "image": "./img/Agric.svg", "name": "Agriculture", "location": "Newcastle", "price": {"GBP": 220}, "spaces": 5}
+]
